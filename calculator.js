2dfdd027b8c49b74f1665a1492486486e7e757ce
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-18 14:53:23 +0100

    implementing addition function

--- calculator.js
+++ calculator.js
@@ -16,12 +16,19 @@ const resetBtn = document.querySelector(".reset-btn");
 const equalsBtn = document.querySelector(".equals-btn");
 const dotBtn = document.querySelector(".dot-btn");
 const acBtn = document.querySelector(".ac-btn");
+const btn = document.querySelectorAll(".btn");
+const calculatorBox = document.querySelector(".calculator");
+const calculatorInput = document.querySelector(".calculator__input");
+const resultValue = document.querySelector(".result-value");
 
-let result;
+let result = 0;
+let a = 0;
+let b = 0;
+let sign = "";
 
 const addition = (a, b) => {
-  result = a + b;
-  return result;
+  result = parseInt(a) + parseInt(b);
+  return (resultValue.innerHTML = result);
 };
 const substraction = (a, b) => {
   result = a - b;
@@ -35,9 +42,52 @@ const multiplication = (a, b) => {
   result = a * b;
   return result;
 };
-const reset = (a, b) => {
+const reset = () => {
   result = 0;
   return result;
 };
 
-console.log(addition(2, 8));
+const clickNumber = (e) => {
+  if (e.target.closest("button").classList.contains("btn-number")) {
+    const clickedButton = e.target.closest("button").textContent;
+    calculatorInput.value += clickedButton;
+  }
+};
+
+// const clickSign = (e) => {
+//   if (e.target.closest("button").classList.contains("btn-sign")) {
+//     const clickedButton = e.target.closest("button").textContent;
+//     console.log(clickedButton);
+//   }
+// };
+
+calculatorBox.addEventListener("click", clickNumber);
+
+plusBtn.addEventListener("click", () => {
+  resultValue.innerHTML = calculatorInput.value;
+
+  sign = "+";
+  result = calculatorInput.value;
+  if (a === 0) {
+    a = result;
+    calculatorInput.value = "";
+  } else {
+    b = calculatorInput.value;
+    addition(result, b);
+    calculatorInput.value = "";
+  }
+  console.log(
+    `+ clicked  a = ${a}   b = ${b}   sign = ${sign}   result = ${result}`
+  );
+});
+
+equalsBtn.addEventListener("click", () => {
+  b = calculatorInput.value;
+  if (sign === "+") {
+    console.log(
+      `= clicked  a = ${a}   b = ${b}   sign= ${sign}   result= ${result}`
+    );
+    addition(result, b);
+    calculatorInput.value = "";
+  }
+});
